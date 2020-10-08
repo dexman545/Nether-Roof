86f9b5977b01702489ae037f475662b0ba8cc4f1
Deximus-Maximus <kev12east@gmail.com>
2020-10-08 13:10:37 -0400

    Update to 1.16.3 and add amerule for roof damage

--- src/main/java/dex/netherroof/NetherRoof.java
+++ src/main/java/dex/netherroof/NetherRoof.java
@@ -2,21 +2,56 @@ package dex.netherroof;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.fabricmc.fabric.api.event.server.ServerTickCallback;
+import net.fabricmc.fabric.api.gamerule.v1.CustomGameRuleCategory;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.Material;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.text.LiteralText;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.GameRules;
+import net.minecraft.world.dimension.DimensionType;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Objects;
+
 public class NetherRoof implements ModInitializer {
 
     public static Logger LOGGER = LogManager.getLogger();
+    public static final CustomGameRuleCategory GREEN_CATEGORY = new CustomGameRuleCategory(new Identifier("netherroof", "dodeath"),
+            new LiteralText("Void Nether Roof Mod").styled(style -> style.withBold(true).withColor(Formatting.DARK_GREEN)));
 
     public static final String MOD_ID = "netherroof";
     public static final String MOD_NAME = "NetherRoofMod";
+    public static final GameRules.Key<GameRules.BooleanRule> DO_DEATH = register("killOnRoof", GREEN_CATEGORY, GameRuleFactory.createBooleanRule(false));
+
+    private static <T extends GameRules.Rule<T>> GameRules.Key<T> register(String name, CustomGameRuleCategory category, GameRules.Type<T> type) {
+        return GameRuleRegistry.register(name, category, type);
+    }
 
     @Override
     public void onInitialize() {
         log(Level.INFO, "Initializing");
-        //TODO: Initializer
+
+        ServerTickEvents.END_SERVER_TICK.register(minecraftServer -> {
+            minecraftServer.getPlayerManager().getPlayerList().forEach(playerEntity -> {
+                if (Objects.requireNonNull(playerEntity.getServer()).getGameRules().getBoolean(DO_DEATH) && playerEntity.getServerWorld().getDimension().hasCeiling() && !playerEntity.isCreativeLevelTwoOp()) {
+                    Vec3d v = playerEntity.getPos();
+                    if (v.y >= 128) {
+                        playerEntity.damage(DamageSource.OUT_OF_WORLD, 5f);
+                    }
+                }
+            });
+        });
     }
 
     public static void log(Level level, String message){
