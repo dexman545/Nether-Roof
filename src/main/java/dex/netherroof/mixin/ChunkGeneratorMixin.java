a4cf02516ac401d4370c0c2a6632b1e87c7aea28
SalveMundiProd <salvemundiproductions@gmail.com>
2021-10-24 07:14:43 -0400

    moved ChunkGeneratorMixin injection to BiomeAccess, parent of MultiNoiseBiomePoint; changed injection from HEAD to RETURN; Added new SpawnBehaviorMixin injection which prevents mobs from spawning if user is in a Nether or Void biome and spawn position in question is above nether roof height; changed default player hurt value to true

--- src/main/java/dex/netherroof/mixin/ChunkGeneratorMixin.java
+++ src/main/java/dex/netherroof/mixin/ChunkGeneratorMixin.java
@@ -1,30 +1,25 @@
 package dex.netherroof.mixin;
+import static dex.netherroof.NetherRoof.*;
 
-import com.mojang.datafixers.util.Pair;
-import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.BiomeKeys;
-import net.minecraft.world.biome.source.MultiNoiseBiomeSource;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.world.biome.source.BiomeAccess;
+
+import static net.minecraft.world.biome.source.BiomeCoords.*;
+
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Optional;
-
-@Mixin(MultiNoiseBiomeSource.class)
+@Mixin(BiomeAccess.class)
 public abstract class ChunkGeneratorMixin {
-
-    @Shadow @Final private Optional<Pair<Registry<Biome>, MultiNoiseBiomeSource.Preset>> instance;
-
-    @Inject(at = @At("HEAD"), method = "getBiomeForNoiseGen(III)Lnet/minecraft/world/biome/Biome;", cancellable = true)
-    public void changeChunk(int biomeX, int biomeY, int biomeZ, CallbackInfoReturnable<Biome> cir) {
-        if (biomeY >= (128 >> 2)) {
-            this.instance.ifPresent(registryPresetPair -> {
-                cir.setReturnValue(registryPresetPair.getFirst().get(BiomeKeys.THE_VOID));
-            });
-        }
+	
+	@Inject(at = @At("RETURN"), method = "getBiomeForNoiseGen(III)Lnet/minecraft/world/biome/Biome;", cancellable = true)
+    public void changeBiomeLoc(int biomeX, int biomeY, int biomeZ, CallbackInfoReturnable<Biome> cir) {
+		if (cir.getReturnValue() != null && cir.getReturnValue().getCategory() == Biome.Category.NETHER && biomeY >= fromBlock(NETHER_HEIGHT)) {
+			cir.setReturnValue(BuiltinRegistries.BIOME.get(BiomeKeys.THE_VOID));
+		}
     }
 }
